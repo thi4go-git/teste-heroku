85526111f5767bfbee01d44e1d9b5e8ae513d2e1
Thiago Junior Amorim De Melo (GOVBR GO - DRC Central) <thiago.melo@govbr.com.br>
2022-12-08 10:18:24 -0300

    Heroku config

--- src/main/java/com/heroku/TesteHerokuApplication.java
+++ src/main/java/com/heroku/TesteHerokuApplication.java
@@ -2,12 +2,19 @@ package com.heroku;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
+@RestController
 public class TesteHerokuApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(TesteHerokuApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(TesteHerokuApplication.class, args);
+    }
 
+    @GetMapping
+    public String hello() {
+        return "Olá";
+    }
 }
