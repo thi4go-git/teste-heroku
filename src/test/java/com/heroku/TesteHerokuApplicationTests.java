6e3212c35a8c040638ddbf0a113c45236ea7bdb9
Thiago Junior Amorim De Melo (GOVBR GO - DRC Central) <thiago.melo@govbr.com.br>
2022-12-08 10:12:06 -0300

    Commit inicial

---
+++ src/test/java/com/heroku/TesteHerokuApplicationTests.java
@@ -0,0 +1,13 @@
+package com.heroku;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TesteHerokuApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
